ef2ce32a9c121f6192ce0f8886990c379e899809
JoseYag <josefelix.yag@gmail.com>
2019-03-21 00:04:55 +0100

    6 acabado

--- practica_2/practica2_6.sh
+++ practica_2/practica2_6.sh
@@ -1,18 +1,44 @@
 #!/bin/bash
 # Autores:  Hayk Kocharyan (757715) & Jose Felix Yagüe Royo (755416)
-# Last update: 10/3/19
+# Last update: 20/3/19
 
 
 
-ruta=~
-dir=$(ls -l "~" | egrep -c '^d' | egrep -o "bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]")
-files=$(ls -l)
 
-if [ $dir = 0]
-	then
+dir=$(ls -l ~ | egrep -c -E '^d.*bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]$')
+files=$(ls -l | egrep -c '^-.*x')
+
+if [ $dir = 0 ]
+then
 #no existe
-	
-	else
+	dtemp=$(mktemp -d binXXX -p ~)
+	#dtemp=$(ls ~ | grep '^bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]$')
+else
 #existe
-	
-	fi 
+	bin=$(ls -tp ~ | egrep 'bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]/$' | tail -1)
+	echo $bin
+	home="/home/"
+	usr=$(whoami)
+	barra="/"
+	dtemp="$home$usr$barra$bin"
+	echo $dtemp
+fi 
+echo el directorio es $dtemp 
+lista=$(ls -p |grep -v /)
+cont=0
+for var in $lista
+do
+	if test -x $var
+	then 
+		cp $var $dtemp
+		echo $var ha sido copiado a $dtemp
+		((cont++))
+
+	fi
+done
+if [ $cont = 0 ]
+then 
+	echo No se ha copiado ningun archivos
+else 
+	echo Se han copiado $cont archivos
+fi
